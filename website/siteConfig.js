680696b762cb0cfac3c76ba26e0a93b94cf11b65
kctanaka <kctanaka@ucdavis.edu>
2020-10-02 12:15:15 -0700

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [];
 
 const siteConfig = {
     title: 'Kimberly Tanaka', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    tagline: 'I like building things!',
     profilePhoto: 'img/profile.png',
     url: 'https://kctanaka.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
